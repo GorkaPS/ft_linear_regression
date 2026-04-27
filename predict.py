38f7692d7d7715b2a499c34c5b18002fa7c61d45
Gorka <gorkapinillaeij@gmail.com>
2026-04-27 22:42:56 +0200

    Train y predict terminado!

---
+++ predict.py
@@ -0,0 +1,54 @@
+
+import json
+from utils import estimate_price 
+import sys
+
+def main():
+	if len(sys.argv) == 2 and sys.argv[1] in ("--help", "-h"):
+		return help_message()
+	if len(sys.argv) != 2:
+		print("uso: python3 predict.py kilometers")
+		return
+	if not sys.argv[1].isnumeric():
+		print("Argument 2 must be a positive number")
+		return
+	for i,arg in enumerate(sys.argv):
+		if len(arg) == 0:
+			print(f"Argumento {i} inválido, string vacío.")
+			return
+	try:
+		with open('thetas.json', 'r') as json_file:
+			thetas = json.load(json_file)
+		required = ["theta1", "theta0"]
+		missing = [key for key in required if key not in thetas]
+		if missing:
+			print("Faltan claves o valores:", missing)
+			return
+		if thetas["theta0"]:
+			theta0 = thetas['theta0']
+		if thetas["theta1"]:
+			theta1 = thetas['theta1']
+	except FileNotFoundError:
+		print(f"Error: no existe el archivo {'thetas.json'}")
+		return
+	except PermissionError:
+		print(f"Error: sin permisos para {'thetas.json'}")
+		return
+	
+	predicted_price = round(estimate_price(theta0, theta1, int(sys.argv[1]) / 1000), 2)
+	if predicted_price < 0:
+		predicted_price = 0
+	print(f"Precio estimado para {sys.argv[1]} km es: {predicted_price}$")
+
+
+
+def help_message():
+	print("usage: ft_turing [-h] jsonfile input\n")
+	print("positional arguments:")
+	print("\tjsonfile	json description of the machine\n")
+	print("\tinput		input of the machine\n")
+	print("optional arguments:")
+	print("\t-h, --help	show this help message and exit\n")
+
+if __name__ == "__main__":
+	main()
--- train.py
+++ train.py
@@ -10,28 +10,32 @@ def main():
 	
 	theta0 = 0
 	theta1 = 0
-	learningrate = 0.00001
+	learningrate = 0.0001
 	with open('data.csv', 'r') as file:
 		reader = csv.DictReader(file)
 		for row in reader:
 			data.append((float(row['km']) / 1000, float(row['price'])))
-	for n in range(3000):
+	for n in range(300000):
 		errortheta0 = 0
 		errortheta1 = 0
 		for mileage, price in data:
-			errortheta0 = errortheta0 + ((estimate_price(theta0, theta1, mileage) - price) / len(data) * 1000)
+			errortheta0 = errortheta0 + ((estimate_price(theta0, theta1, mileage) - price) / len(data))
 			errortheta1 = errortheta1 + (((estimate_price(theta0, theta1, mileage) - price) * mileage ) / len(data))
-			
-		print ("iter", n)
-		print(f"theta0= " + str(theta0))
-		print(f"theta1= " + str(theta1))
 		theta0 = theta0 - (learningrate * errortheta0)
 		theta1 = theta1 - (learningrate * errortheta1)
-	print (theta0)
-	print (theta1)
-
-
-
+	thetas = dict(
+		theta0= theta0,
+		theta1 = theta1)
+	try:
+		with open('thetas.json', 'w') as json_file:
+			json.dump(thetas, json_file,  indent=2)
+	except FileNotFoundError:
+		print(f"Error: no existe el archivo {'thetas.json'}")
+		return
+	except PermissionError:
+		print(f"Error: sin permisos para {'thetas.json'}")
+		return
+	print("Archivo JSON generado correctamente")
 
 if __name__ == "__main__":
 	main()
